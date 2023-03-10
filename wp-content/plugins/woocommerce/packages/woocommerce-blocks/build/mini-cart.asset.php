edee431a820cdc560e4f4151d1ca117ff5788d84
Creode Developers <dev@creode.co.uk>
2023-03-10 12:31:06 +0000

    Updated plugin Woocommerce from 7.0.0 to 7.4.1.

--- wp-content/plugins/woocommerce/packages/woocommerce-blocks/build/mini-cart.asset.php
+++ wp-content/plugins/woocommerce/packages/woocommerce-blocks/build/mini-cart.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-price-format', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'd3acbcbc2075dc160f956648adcbb4ee');
+<?php return array('dependencies' => array('react', 'wc-price-format', 'wc-settings', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '77a16e8a2d8e1cc493e7bffc0e374c76');
